2ca3d11aed2f1f9859bea4e5206487224d375273
Rodion <oellekon@mail.ru>
2020-06-19 17:42:27 +0300

    Fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('about', views.AboutPage.as_view(), name='about'),
+    path('about/', views.AboutPage.as_view(), name='about'),
 ]
