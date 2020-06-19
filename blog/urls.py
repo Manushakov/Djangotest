cd43fd13a9273ee13d8864b83b0fbde64a6b71d5
Rodion <oellekon@mail.ru>
2020-06-19 16:58:05 +0300

    Replace templates, made Class-Based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.HomePage.as_view(), name='home'),
+    path('about', views.AboutPage.as_view(), name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+class HomePage(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPage(TemplateView):
+    template_name = 'about.html'
